94e93ef940a158be71089c314aae2b3f412dfc37
cupofnestor <nestor@fuzzy-math.com>
2013-07-05 15:10:43 -0400

    Update ww_startup.bat

--- ww_startup.bat
+++ ww_startup.bat
@@ -1,4 +1,4 @@
 chdir C:\%HOMEPATH%\WhosWho
-START C:\Python27\python.exe C:\%HOMEPATH%\WhosWho\server.py
+START C:\Python27\python.exe server.py
 timeout /t 2
 START C:\%HOMEPATH%\AppData\Local\Google\Chrome\Application\chrome.exe http://localhost:8080/static/who.html  --user-data-dir=$(mktemp -d)   --kiosk
